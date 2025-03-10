8956e2e6d10725eb563ba8320c3783d8d7fd79b3
Tiltediedixiss <gajaparov@gmail.com>
2025-03-10 14:14:29 +0500

    better model

--- model.py
+++ model.py
@@ -1,7 +1,16 @@
 import tensorflow as tf
 from keras import datasets, layers, models 
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
 import matplotlib.pyplot as plt
 import numpy as np
+datagen = ImageDataGenerator(
+    rotation_range=15,
+    width_shift_range=0.05, 
+    height_shift_range=0.05, 
+    horizontal_flip=True,
+    zoom_range=0.1
+)
 (X_train, y_train), (X_test, y_test) = datasets.cifar10.load_data()
 classes=["airplane", "automobile", "bird", "cat", "dear", "dog", "frog", "house", "ship", "truck"]
 y_train = y_train.reshape(-1,)
@@ -30,23 +39,28 @@ X_test=X_test/255
 #ann.evaluate(X_test, y_test)
 
 cnn = models.Sequential([
-    #cnn
-    layers.Conv2D(filters=32, activation='relu', kernel_size=(3,3), input_shape=(32,32,3)),
-    layers.MaxPooling2D((2,2)),
-
     layers.Conv2D(filters=64, activation='relu', kernel_size=(3,3), input_shape=(32,32,3)),
+    layers.BatchNormalization(),
     layers.MaxPooling2D((2,2)),
 
-    layers.Conv2D(filters=128, activation='relu', kernel_size=(3,3), input_shape=(32,32,3)),
+    layers.Conv2D(filters=128, activation='relu', kernel_size=(3,3)),
+    layers.BatchNormalization(),
     layers.MaxPooling2D((2,2)),
-    #dense
+
+    layers.Conv2D(filters=256, activation='relu', kernel_size=(3,3)),
+    layers.BatchNormalization(),
+    layers.MaxPooling2D((2,2)),
+
     layers.Flatten(),
-    layers.Dense(64, activation='relu'),
+    layers.BatchNormalization(),
+    layers.Dense(256, activation='relu'),
     layers.Dense(10, activation='softmax')
 ])
-cnn.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+optimizer = Adam(learning_rate=0.0001)
+cnn.compile(optimizer= optimizer, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-cnn.fit(X_train, y_train, epochs=10)
+datagen.fit(X_train)
+cnn.fit(datagen.flow(X_train, y_train, batch_size=64), epochs=20, validation_data=(X_test, y_test))
 
 cnn.evaluate(X_test, y_test)
 
@@ -62,4 +76,4 @@ for i in range(10):  # Show first 10 images
     plt.axis('off')
     plt.show()
 
-cnn.save('cifar10_model.h5')
+cnn.save('cifar10_model.h5')    
